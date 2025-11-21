8b29837846a886f0134675ca5b339ad6461b0ff4
Enes Emre Eravci <eeravci@c3r14s1.42warsaw.pl>
2025-11-21 17:10:21 +0100

    ex00 is done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,21 @@
-
-
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
-    // not know the task yet
+    if(argc >= 2)
+    {
+        for(int i = 1; i < argc; ++i)
+        {
+            for(int j = 0; argv[i][j] != '\0'; ++j)
+            {
+                std::cout << (char)toupper((unsigned char)argv[i][j]);
+            }
+        }  
+        std::cout << "\n";
+    }
+    else
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+    }
 }
---
+++ ex01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+
+
+int main(int argc, char **argv)
+{
+
+}

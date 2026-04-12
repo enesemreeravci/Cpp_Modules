3c56026124ac4d5217b1ba293e014701668908ab
Enes Emre Eravci <enes.nev@gmail.com>
2026-04-12 14:44:03 +0200

    ex03 fixed and finished

--- module1/ex03/HumanB.cpp
+++ module1/ex03/HumanB.cpp
@@ -5,6 +5,10 @@ HumanB::HumanB(std::string name) : name(name)
     std::cout << "human name: "  << name <<  "created: " << WeaponB->getType() << '\n';
 }
 
+HumanB::~HumanB()
+{
+
+}
 void HumanB::attack()
 {
     std::cout << "human name: " << name << "now attacking with a weapon: " << WeaponB->getType() << '\n';

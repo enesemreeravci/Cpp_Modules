afbd5d749ae1bb6ea38e3bb1c3b7a1f3222d109a
Enes Emre Eravci <enes.nev@gmail.com>
2026-04-25 15:01:18 +0200

    ex03 is ongoing

---
+++ module2/ex02/Fixed.hpp
@@ -0,0 +1,41 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+    int numberFixedPoint;
+    static const int numberOfFractionalBits;
+
+    public:
+    Fixed(); // default const
+    Fixed(const int value_int);
+    Fixed(const float value_float);
+    Fixed(const Fixed& other); // copy const
+    Fixed& operator=(const Fixed& other); // copy assignment operator
+    ~Fixed(); // deconstructor
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat( void ) const;
+    int toInt( void ) const;
+
+    // The 6 comparison operators: >, <, >=, <=, ==, and !=
+
+    bool operator>(const Fixed& other) const;
+    bool operator<(const Fixed& other) const;
+    bool operator>=(const Fixed& other) const;
+    bool operator<=(const Fixed& other) const;
+    bool operator==(const Fixed& other) const;
+    bool operator!=(const Fixed& other) const;
+    
+
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed_num);
+
+#endif 

52c2a1c43e3a658ed6e9ef7fb485701bc99c9091
enesemreeravci <enes.nev@gmail.com>
2026-05-17 19:40:59 +0200

    module4

---
+++ module4/ex00/Animal.hpp
@@ -0,0 +1,24 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+    protected:
+        std::string type;
+
+    public:
+        Animal();
+        Animal(const std::string type);
+        Animal(const Animal& rhs);
+        Animal& operator=(const Animal& rhs);
+        ~Animal();
+
+        virtual void makeSound() const;
+        std::string getType() const;
+};
+
+
+#endif

79b3d891a614d7e92d5b02ea7c6f99b2a272fa25
enesemreeravci <enes.nev@gmail.com>
2026-05-25 14:52:51 +0200

    ex03 started

---
+++ module4/ex03/AMateria.hpp
@@ -0,0 +1,28 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <iostream>
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+    protected:
+        std::string type;
+
+    public:
+        AMateria();
+        AMateria(const std::string& type);
+        AMateria(const AMateria& rhs);
+        AMateria& operator=(const AMateria& rhs);
+        virtual ~AMateria();
+
+        const std::string& getType() const;
+        virtual void use(ICharacter& target);
+        virtual AMateria* clone() const = 0; 
+        // we forced every derived class
+        // inherits from this class must implement/override this function
+
+    };
+
+#endif
---
+++ module4/ex03/ICharacter.hpp
@@ -0,0 +1,23 @@
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include <iostream>
+#include <string>
+#include "AMateria.hpp"
+
+class ICharacter
+{
+    protected:
+        ICharacter();
+        ICharacter(const ICharacter& rhs);
+        ICharacter& operator=(const ICharacter& rhs);
+    public:
+        virtual ~ICharacter() {};
+        virtual const std::string& getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
+};
+
+
+#endif

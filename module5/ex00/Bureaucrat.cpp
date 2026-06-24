3d420b0d2c3b466ffa898454fb787accf5fcef8e
enesemreeravci <enes.nev@gmail.com>
2026-06-24 18:48:50 +0200

    module5 ex00

---
+++ module5/ex00/Bureaucrat.cpp
@@ -0,0 +1,77 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("default")
+{
+    this->grade = 150;
+    std::cout << "default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade)
+{
+    if(grade > 150)
+        throw GradeTooHighException();
+    else if (grade < 1) 
+        throw GradeTooLowException();
+    std::cout << "Paramatrized constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy)
+{
+    *this = copy;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+    if(this != &other)
+    {
+        this->grade = other.grade;
+    }
+    std::cout << "Copy assignemnt operator called" << std::endl;
+    return *this;
+}
+
+std::string Bureaucrat::getName() const
+{
+    return name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return grade;
+}
+
+void Bureaucrat::inc()
+{
+    if (grade == 1)
+        throw GradeTooHighException();
+    grade--;
+}
+
+void Bureaucrat::dec()
+{
+    if (grade == 150)
+        throw GradeTooLowException();
+    grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return ("Exception: The highest possible grade is 1");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return ("Exception: The lowrst possible grade is 150");
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj)
+{
+    os << obj.getName() << ", bureacrat grade: " << obj.getGrade() << std::endl;
+    return os;
+}
